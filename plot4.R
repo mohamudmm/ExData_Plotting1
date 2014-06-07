789324f2ced611fc9d03beda2b2bb029003f6f9d
mohamudmm <mohamudmm@gmail.com>
2014-06-07 05:50:14 -0500

    updated all files

--- plot4.R
+++ plot4.R
@@ -1,5 +1,5 @@
 #reads the data from a file in the working directory
-exdata2 <- read.csv2("//household_power_consumption.txt",header = FALSE, nrow= 2880, skip = 66637,sep = ";")
+exdata2 <- read.csv2("household_power_consumption.txt",header = FALSE, nrow= 2880, skip = 66637,sep = ";")
 #create a vector for the column names
 cnames <- c('Date','Time','Global_active_power','Global_reactive_power','Voltage','Global_intensity','Submetering_1','submetering_2','submetering_3')
 #attach the column names to the dataframe
